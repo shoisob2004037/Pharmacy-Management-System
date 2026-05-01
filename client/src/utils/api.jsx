626dffc21410927c090da45b9bf869ebe3aefba7
shoisob2004037 <mhshoisob@gmail.com>
2026-05-01 21:52:54 +0600

    k

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const baseURL = "https://pharmacy-management-system-flame.vercel.app/api" || "http://localhost:5000/api"
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
